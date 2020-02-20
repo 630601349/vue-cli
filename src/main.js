c184f337c077a742679b25f944f5aca13d881337
oubailiang <630601349@qq.com>
2020-02-20 09:48:17 +0800

    创建并引入ElementUI组件库

--- src/main.js
+++ src/main.js
@@ -7,14 +7,19 @@ import axios from 'axios'
 //引入MinUi库，配置
 import MinUi from 'mint-ui'
 
+import ElementUI from 'element-ui'
+
 //引入MinUi库样式文件
 import "mint-ui/lib/style.css"
 import MyHeader from './components/obj1/myHeader'
 import MyFoot from './components/obj1/myFoot'
+import "element-ui/lib/theme-chalk/index.css"
 
 Vue.component("myheader",MyHeader)
 
 Vue.component("myfoot",MyFoot)
+
+Vue.use(ElementUI)
 
 //将组件库注册Vue对象
 Vue.use(MinUi);
